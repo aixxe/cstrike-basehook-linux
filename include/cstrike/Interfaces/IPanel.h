dc524f61aac5ff987d2b97ea258f4977fcfa78bb
aixxe <me@aixxe.net>
2016-12-19 18:29:20 +0000

    Fix incorrect virtual index for IPanel::PaintTraverse.
    Signed-off-by: aixxe <me@aixxe.net>

--- include/cstrike/Interfaces/IPanel.h
+++ include/cstrike/Interfaces/IPanel.h
@@ -9,6 +9,6 @@ class IPanel {
 		}
 
 		void PaintTraverse(VPANEL vpanel, bool force_repaint, bool allow_force) {
-			GetVirtualFunction<void(*)(IPanel*, VPANEL, bool, bool)>(this, 37)(this, vpanel, force_repaint, allow_force);
+			GetVirtualFunction<void(*)(IPanel*, VPANEL, bool, bool)>(this, 42)(this, vpanel, force_repaint, allow_force);
 		}
 };
