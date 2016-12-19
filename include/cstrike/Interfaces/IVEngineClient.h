cdd6c28dac856272b8dd16eaf12b97f739723700
aixxe <me@aixxe.net>
2016-12-19 18:15:59 +0000

    Add IVEngineClient interface.
    Signed-off-by: aixxe <me@aixxe.net>

---
+++ include/cstrike/Interfaces/IVEngineClient.h
@@ -0,0 +1,52 @@
+#pragma once
+
+class IVEngineClient {
+	public:
+		bool Con_IsVisible() {
+			return GetVirtualFunction<bool(*)(IVEngineClient*)>(this, 11)(this);
+		}
+
+		int GetLocalPlayer() {
+			return GetVirtualFunction<int(*)(IVEngineClient*)>(this, 12)(this);
+		}
+
+		float Time() {
+			return GetVirtualFunction<float(*)(IVEngineClient*)>(this, 14)(this);
+		}
+
+		float GetLastTimeStamp() {
+			return GetVirtualFunction<float(*)(IVEngineClient*)>(this, 15)(this);
+		}
+				
+		void GetViewAngles(QAngle& angles) {
+			return GetVirtualFunction<void(*)(IVEngineClient*, QAngle&)>(this, 19)(this, angles);
+		}
+
+		void SetViewAngles(QAngle& angles) {
+			return GetVirtualFunction<void(*)(IVEngineClient*, QAngle&)>(this, 20)(this, angles);
+		}
+
+		int GetMaxClients() {
+			return GetVirtualFunction<int(*)(IVEngineClient*)>(this, 21)(this);
+		}
+
+		bool IsInGame() {
+			return GetVirtualFunction<bool(*)(IVEngineClient*)>(this, 26)(this);
+		}
+
+		bool IsConnected() {
+			return GetVirtualFunction<bool(*)(IVEngineClient*)>(this, 27)(this);
+		}
+
+		const char* GetGameDirectory() {
+			return GetVirtualFunction<const char*(*)(IVEngineClient*)>(this, 35)(this);
+		}
+
+		const char* GetLevelName() {
+			return GetVirtualFunction<const char*(*)(IVEngineClient*)>(this, 51)(this);
+		}
+
+		void ClientCmd_Unrestricted(const char* command) {
+			return GetVirtualFunction<void(*)(IVEngineClient*, const char*)>(this, 106)(this, command);
+		}
+};
