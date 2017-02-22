0eb481e75452341e2a859ebbfbf4e8114d737ef9
aixxe <me@aixxe.net>
2017-02-22 15:25:25 +0000

    Remove duplicate GetVirtualFunction.
    * Thanks to @luk1337 for pointing this out.
    
    Signed-off-by: aixxe <me@aixxe.net>

--- include/cstrike/Utilities/Virtuals.h
+++ include/cstrike/Utilities/Virtuals.h
@@ -4,10 +4,6 @@ inline void**& GetVirtualTable(void* baseclass) {
 	return *reinterpret_cast<void***>(baseclass);
 }
 
-inline void* GetVirtualFunction(void* vftable, size_t index) {
-	return reinterpret_cast<void*>(GetVirtualTable(vftable)[index]);
-}
-
-template <typename Fn> inline Fn GetVirtualFunction(void* vftable, size_t index) {
-	return reinterpret_cast<Fn>(GetVirtualTable(vftable)[index]);
+template <typename Fn = void*> inline Fn GetVirtualFunction(void* baseclass, size_t index) {
+	return reinterpret_cast<Fn>(GetVirtualTable(baseclass)[index]);
 }
